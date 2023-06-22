284195b8e23aa279fb7fbba60f0be354513cb576
faiyazAziz <106823454+faiyazAziz@users.noreply.github.com>
2023-06-23 01:19:39 +0530

    Update salaryPrediction.py

--- salaryPrediction.py
+++ salaryPrediction.py
@@ -1,8 +1,10 @@
 import math
-import base64
+import numpy as np
 import pandas as pd
-import streamlit
+import matplotlib.pyplot as plt
 import streamlit as st
+from numerize.numerize import numerize
+
 
 df = pd.read_csv("salaryData.csv", skipinitialspace=True)
 all_companies = df['Company Name'].unique()
@@ -25,32 +27,61 @@ def find_salary(company_name,job_title,location,employment_status,job_roles):
 
     return salary,rating
 
+def ratio():
+    lst = []
+    lst2 = []
+    lst3 = []
+    for i in range(len(all_job_roles)):
+        job = all_job_roles[i]
+        lst2.append(job)
+        aa = df[df['Job Roles']==job]
+        count = aa.shape[0]
+        lst.append(count)
+        lst3.append(0)
+    return lst,lst2,lst3
+
+def compare_company(options):
+    lst = []
+    for i in range(len(options)):
+        aa = df[df['Company Name']==options[i]]
+        count = aa.shape[0]
+        lst.append(count)
+    return lst
+
+
 # salaryv,r = find_salary('Sasken','Android Developer','Bangalore','Full Time','Android')
 # print(salaryv)
 
-st.markdown('''
-            <meta charset = "UTF-8">
-            <h3 ><i>Welcome To </i>
-            <font size="7" color="red" face="Optima">SalaryJano &#129297	
-            </font></h3>
-            ''',unsafe_allow_html=True)
+# st.title("**Welcome To** SalaryJano :sunglasses:")
 # if "button_clicked" not in st.session_state:
 #     st.session_state.button_clicked = False
 #
 # def callback():
 #     st.session_state.button_clicked = True
 
+
+st.markdown('''
+            <meta charset = "UTF-8">
+            <h3 style = "vertical-align:text-top"><i>Welcome To </i>
+            <font size="7" color="red" face="Optima">SalaryJano &#129297	
+            </font></h3>
+            ''',unsafe_allow_html=True)
+
 with st.sidebar:
-    add_radio = st.radio("What do you want to do",("Predict Salary","Analyze Company","Compare Company"))
+    add_radio = st.radio("What do you want to do",("Predict Salary","Analyze Company","Compare Company","Ratio of Job"))
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 
 if(add_radio == "Predict Salary"):
+    st.header("Predict Your Salary")
     company = st.selectbox('Chose Your Dream Company',all_companies)
     job_title = st.selectbox('Choose Job Title',all_job_titles)
     location = st.selectbox('Choose Location',all_loaction)
     employment_status = st.selectbox('Choose Status',all_employment_status)
     job_role = st.selectbox('Choose Job Role',all_job_roles)
 
-    submit = st.button('SUBMIT')
+    submit = st.button('SALARY')
 
     if(submit):
         salary, rating = find_salary(company,job_title,location,employment_status,job_role)
@@ -60,4 +91,34 @@ if(add_radio == "Predict Salary"):
                 st.text("NO DATA FOUND")
                 st.image('NoData.png')
         else:
-            tt =st.write(salary)
+            st.text("")
+            st.text("")
+            st.write(u'\u20B9'+ " " +numerize(salary))
+
+
+if(add_radio == "Ratio of Job"):
+    st.header("Pie Chart of Tech Jobs In India")
+    y,labels,explode = ratio()
+    y = np.array(y)
+    labels = np.array(labels)
+    # explode[0]=0.2
+    explode[7]=0.1
+    fig,ax = plt.subplots()
+    ax.pie(y,labels=labels,shadow=True,explode=explode)
+    st.pyplot(fig,use_container_width=True)
+
+if(add_radio=='Compare Company'):
+    options = st.multiselect("Select Company To Compare",all_companies,max_selections=4)
+    compare = st.button("COMPARE")
+    if(compare):
+        st.text("")
+        st.text("")
+        st.text("")
+        st.text("")
+        y = compare_company(options)
+        fig,ax = plt.subplots()
+        ax.bar(options,y)
+        st.pyplot(fig,use_container_width=True)
+
+
+
